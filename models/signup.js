d60ced4f1f7468fe67081a56c3260cc971b20169
Shi Hao Tan <shihaotan91@gmail.com>
2016-10-24 14:56:22 +0800

    bcrypt working

--- models/signup.js
+++ models/signup.js
@@ -1,10 +1,50 @@
 var mongoose = require('mongoose')
+var bcrypt = require('bcrypt')
 
-var userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String
+var userSchema = mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
 })
+
+userSchema.pre('save', function (next) {
+  // console.log('before save hash the password')
+  // console.log(this)
+
+  var user = this
+
+  bcrypt.genSalt(5, function (err, salt) {
+    if (err) return next(err)
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err)
+
+      user.password = hash
+      // console.log('after hash')
+      // console.log(user)
+      next()
+    })
+  })
+})
+
+userSchema.methods.authenticate = function (givenPassword, callback) {
+  console.log('given password is ' + givenPassword)
+  console.log('saved password is ' + this.password)
+  var hashedPassword = this.password
+
+  bcrypt.compare(givenPassword, hashedPassword, function (err, isMatch) {
+    callback(err, isMatch)
+  })
+}
 
 var User = mongoose.model('User', userSchema)
 
